e39022bd27c3163b5e27c06213d24791ef52fd50
SparrowHawkEye <makh0043@gmail.com>
2022-03-09 15:28:56 +0600

    fixed favicon

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const deleteIssue = id => {
 
 const fetchIssues = () => {
 
-  const issues = JSON.parse(localStorage.getItem('issues'||[]));
+  const issues = JSON.parse(localStorage.getItem('issues'))||[];
   // console.log(issues);
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
